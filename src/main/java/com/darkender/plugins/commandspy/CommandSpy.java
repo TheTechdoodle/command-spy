0b1978ea815c418f6ba181c239601158bfa6a1ad
TheTechdoodle <admin+github@darkender.com>
2020-04-19 13:17:16 -0700

    Add structure

---
+++ src/main/java/com/darkender/plugins/commandspy/CommandSpy.java
@@ -0,0 +1,12 @@
+package com.darkender.plugins.commandspy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CommandSpy extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+    
+    }
+}
